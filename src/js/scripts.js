1d78c330442c68c26e37d3c9cd67b0da7e0f1905
terlim <terlim@yandex.ru>
2018-12-30 20:34:06 +0300

    navbar

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,13 @@
+//= ../../node_modules/jquery/dist/jquery.js
 //= ../../node_modules/popper.js/dist/umd/popper.js
 
 //= ../../node_modules/bootstrap/dist/js/bootstrap.min.js
+
+$(function () {
+    'use strict'
+
+    $('[data-toggle="offcanvas"]').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+        $('.icon-bar--wrapper').toggleClass('open')
+    })
+})
