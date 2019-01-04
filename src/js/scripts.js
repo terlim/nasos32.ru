5f36e98e53fe4ff445831c6446da5470f02e77e1
terlim <terlim@yandex.ru>
2019-01-04 14:34:00 +0300

    main page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,43 @@
 //= ../../node_modules/popper.js/dist/umd/popper.js
 
 //= ../../node_modules/bootstrap/dist/js/bootstrap.min.js
+//= ../../node_modules/swiper/dist/js/swiper.min.js
 
+$(document).ready(function () {
+    var mySwiper = new Swiper ('#swiper-promo', {
+        // Disable preloading of all images
+        preloadImages: false,
+        // Enable lazy loading
+        lazy: {
+            loadPrevNext: true,
+        },
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        grabCursor: true,
+        effect: 'slide',
+        speed: 1000,
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 4000,
+        },
+
+        // Default parameters
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loadPrevNextAmount: 2,
+
+    });
+});
 $(function () {
     'use strict'
 
